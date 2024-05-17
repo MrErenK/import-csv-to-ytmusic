bfddf3f8935d8df06d5548aabd681b6996b8c64b
MrErenK <akbaseren4751@gmail.com>
2024-05-17 15:29:26 +0300

    Add ability to add the songs to the liked music playlist

--- add_songs_to_ytmusic_playlist.py
+++ add_songs_to_ytmusic_playlist.py
@@ -68,34 +68,42 @@ def authenticate_ytmusic() -> YTMusic:
         exit()
     return ytmusic
 
-def get_playlist_choice() -> bool:
+def get_playlist_choice() -> str:
     while True:
-        playlist_choice = input("Do you want to add the songs to an existing playlist or create a new one? (existing/e or new/n): ")
-        if playlist_choice.lower() in ['existing', 'e', 'ex']:
-            return True
-        elif playlist_choice.lower() in ['new', 'n']:
-            return False
+        choice = input("Do you want to add the songs to an existing playlist, create a new one, or add to Liked Songs? (existing/e, new/n, liked/l): ")
+        if choice.lower() in ['existing', 'e', 'ex', 'exist']:
+            return 'existing'
+        elif choice.lower() in ['new', 'n', 'ne', 'create']:
+            return 'new'
+        elif choice.lower() in ['liked', 'l', 'like', 'likes']:
+            return 'liked'
         else:
-            print("Invalid choice. Please enter 'existing' or 'new'.")
+            print("Invalid choice. Please enter 'existing', 'new', or 'liked'.")
 
-def create_or_get_playlist(ytmusic: YTMusic) -> str:
-    if get_playlist_choice():
+def create_or_get_playlist(ytmusic: YTMusic, values) -> str:
+    choice = get_playlist_choice()
+    if choice == 'liked':
+        return get_liked_playlist(ytmusic)
+    elif choice == 'existing':
         return get_existing_playlist(ytmusic)
-    else:
+    elif choice == 'new':
         return create_playlist(ytmusic)
 
 def create_playlist(ytmusic: YTMusic) -> str:
     playlist_name = input("Enter the playlist name to create: ")
     playlist_description = input("Enter a description for the playlist: ")
     try:
-        ytmusic.create_playlist(playlist_name, description=playlist_description)
+        playlist_id = ytmusic.create_playlist(playlist_name, description=playlist_description)
     except Exception as e:
         print(f"Error creating playlist: {e}")
         exit()
     print(f"New playlist '{playlist_name}' created.")
-    return playlist_name
+    return playlist_id
 
 def get_existing_playlist(ytmusic: YTMusic, prompt: str = "Enter the number of the playlist to add the songs to: ") -> str:
+    if not ytmusic:
+        print("Error: YTMusic object not found.")
+        exit()
     try:
         playlists = [playlist for playlist in ytmusic.get_library_playlists() if playlist['playlistId'] not in PLAYLIST_IDS_TO_NOT_SHOW]
     except Exception as e:
@@ -110,7 +118,7 @@ def get_existing_playlist(ytmusic: YTMusic, prompt: str = "Enter the number of t
     while True:
         playlist_number = input(prompt)
         if playlist_number.isdigit() and 1 <= int(playlist_number) <= len(playlists):
-            return playlists[int(playlist_number) - 1]['title']
+            return playlists[int(playlist_number) - 1]['playlistId']
         else:
             print("Invalid input. Please enter a valid number.")
 
@@ -139,6 +147,74 @@ def get_playlist_songs(ytmusic: YTMusic, playlist_id: str) -> list:
         print(f"Error getting songs from playlist: {e}")
         exit()
 
+def get_playlist_name(ytmusic: YTMusic, playlist_id: str) -> str:
+    try:
+        playlists = ytmusic.get_library_playlists()
+    except Exception as e:
+        print(f"Error getting library playlists: {e}")
+        exit()
+    playlist_name = None
+    for playlist in playlists:
+        if playlist['playlistId'] == playlist_id:
+            playlist_name = playlist['title']
+            break
+    if playlist_name is None:
+        print("Error: The specified playlist ID does not exist.")
+        exit()
+    return playlist_name
+
+def get_liked_playlist(ytmusic: YTMusic) -> str:
+    try:
+        return 'LM'
+    except Exception as e:
+        print("Error: Liked Songs playlist not found.")
+        exit()
+
+def add_to_liked_songs(ytmusic: YTMusic, values: list) -> None:
+    try:
+        confirmation = input("Are you sure you want to add these songs to Liked Songs? (yes/no): ").strip().lower()
+        if confirmation not in ["yes", "y"]:
+            print("Operation canceled.")
+            return
+
+        liked_songs = ytmusic.get_liked_songs(limit=None)
+        liked_song_ids = {song['videoId']: song['title'] for song in liked_songs['tracks']}
+        total_liked_songs = len(liked_songs['tracks'])
+
+        print(f"Total number of songs in Liked Songs: {total_liked_songs}")
+        print("Adding songs to Liked Songs...")
+
+        added_song_count = 0
+        for value in values:
+            if value in liked_song_ids:
+                print(f"Song: {liked_song_ids[value]} is already in Liked Songs. Skipping...")
+                continue
+            try:
+                song_info = ytmusic.get_song(value)
+                if 'videoDetails' not in song_info:
+                    print(f"Error: 'videoDetails' not found for song with ID {value}")
+                    continue
+
+                song_details = song_info['videoDetails']
+                song_title = song_details.get('title', 'Unknown Title')
+                song_author = song_details.get('author', 'Unknown Author')
+                song_id = song_details.get('videoId', 'Unknown ID')
+
+                print(f"Song: {song_title}, Artist: {song_author}")
+                print(f"URL: https://music.youtube.com/watch?v={song_id}")
+                print("Adding to Liked Songs...")
+                print("")
+                ytmusic.rate_song(song_id, 'LIKE')
+                added_song_count += 1
+            except Exception as e:
+                print(f"Error adding song {value} to Liked Songs: {e}")
+
+        print(f"Total number of songs added to Liked Songs: {added_song_count}")
+        print(f"Total number of songs in Liked Songs: {total_liked_songs + added_song_count}")
+    except Exception as e:
+        print(f"Error retrieving liked songs: {e}")
+    print("Finished adding songs to Liked Songs.")
+
 def process_song(ytmusic: YTMusic, value: str, playlist_id: str, playlist_name: str) -> int:
     try:
         song_info = ytmusic.get_song(value)
@@ -158,12 +234,12 @@ def process_song(ytmusic: YTMusic, value: str, playlist_id: str, playlist_name: 
         print(f"Song: {song_title}, Artist: {song_author}")
         print(f"URL: https://music.youtube.com/watch?v={song_id}")
         print(f"Adding to playlist: {playlist_name}, {playlist_id}")
+        print("")
         try:
             ytmusic.add_playlist_items(playlist_id, [song_id])
         except Exception as e:
             print(f"Error adding song {song_title} to playlist: {e}")
             return 0
-        print("")
         return 1
     except Exception as e:
         print(f"Error processing song with ID {value}: {e}")
@@ -229,10 +305,22 @@ def check_duplicates(ytmusic: YTMusic, delete_duplicates: bool) -> None:
         delete_duplicate_song(ytmusic, playlist_id)
 
 def get_playlist_info(ytmusic: YTMusic, values: list, args: argparse.Namespace) -> None:
-    playlist_name = create_or_get_playlist(ytmusic)
-    playlist_id = get_playlist_id(ytmusic, playlist_name)
-    track_count = len(get_playlist_songs(ytmusic, playlist_id))
-    process_values(ytmusic, values, playlist_id, playlist_name, args.delete_duplicates, track_count)
+    if args.add_to_liked:
+        add_to_liked_songs(ytmusic, values)
+    else:
+        choice = get_playlist_choice()
+        if choice == 'liked':
+            add_to_liked_songs(ytmusic, values)
+        elif choice == 'existing':
+            playlist_id = get_existing_playlist(ytmusic)
+        elif choice == 'new':
+            playlist_id = create_playlist(ytmusic)
+        else:
+            raise ValueError("Invalid playlist choice.")
+
+        playlist_name = get_playlist_name(ytmusic, playlist_id)
+        track_count = len(get_playlist_songs(ytmusic, playlist_id))
+        process_values(ytmusic, values, playlist_id, playlist_name, args.delete_duplicates, track_count)
 
 def main() -> None:
     try:
@@ -240,6 +328,7 @@ def main() -> None:
         parser.add_argument('--csv', type=str, help='Path to the CSV file.')
         parser.add_argument('--delete-duplicates', '-dd', action='store_true', help='Delete duplicate songs from the playlist.')
         parser.add_argument('--check-duplicates', '-cd', action='store_true', help='Check for duplicate songs in a playlist.')
+        parser.add_argument('--add-to-liked', '-al', action='store_true', help='Add songs to Liked Songs instead of a playlist.')
         args = parser.parse_args()
 
         ytmusic = authenticate_ytmusic()
@@ -257,6 +346,7 @@ def main() -> None:
             values = list(get_unique_song_ids(df, id_column))
             print(f"Total number of unique songs found in CSV file: {len(values)}")
             get_playlist_info(ytmusic, values, args)
+
     except KeyboardInterrupt:
         print("")
         print("Exiting...")
